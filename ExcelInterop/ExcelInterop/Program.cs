0f82e1482cc4f88d8bb328647b0b65ec8b8f283f
Ali Abdulhussein <Ali.Abdulhussein@flexlink.com>
2019-05-13 13:43:03 +0200

    Run Excel with Task

--- ExcelInterop/ExcelInterop/Program.cs
+++ ExcelInterop/ExcelInterop/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
 
 namespace ExcelInterop
@@ -10,21 +11,29 @@ namespace ExcelInterop
     {
         static void Main(string[] args)
         {
-            var filePath = @"C:\Dev\TestProject\ExcelInterop\Assembly_time_calculator.xlsx";
+            var task = Task.Factory.StartNew((ExcelTask));
+            task.Wait();
+
+            Console.ReadLine();
+        }
+
+        private static void ExcelTask()
+        {
+            var filePath = @"C:\Dev\DotNetTestProject\ExcelInterop\Assembly_time_calculator.xlsx";
 
 
             Application
                 xlAppObject = new Application();
 
-            xlAppObject.Visible = false;//Excel WorkBook Visible mode true , False InVisible  
+            xlAppObject.Visible = false; //Excel WorkBook Visible mode true , False InVisible  
             xlAppObject.ScreenUpdating = true;
 
 
-            Workbook wb = xlAppObject.Workbooks.Open(filePath,Editable:true,IgnoreReadOnlyRecommended:true);
+            Workbook wb = xlAppObject.Workbooks.Open(filePath, Editable: true, IgnoreReadOnlyRecommended: true);
             //Workbook wb = xlAppObject.Workbooks.Open(filePath, 0, false, 5, "", "", false, XlPlatform.xlWindows, "",
             //    true, false, 0, true, false, false);
-            
-            
+
+
             Debug.WriteLine($"Nr of Sheet: {wb.Sheets.Count}");
 
             Sheets sheets = wb.Worksheets;
@@ -37,42 +46,41 @@ namespace ExcelInterop
             //var cellOneValue = excelRange.Item[1, 1].ToString();
             //var cellTwoValue = excelRange.Item[1, 2].ToString();
 
-            var cellOneValue = (string)(worksheet.Cells[5, 3] as Range).Value;
-            var cellTwoValue = (double)(worksheet.Cells[6, 3] as Range).Value;
-            var cellResult = (double)(worksheet.Cells[39, 10] as Range)?.Value;
+            var cellOneValue = (string) (worksheet.Cells[5, 3] as Range).Value;
+            var cellTwoValue = (double) (worksheet.Cells[6, 3] as Range).Value;
+            var cellResult = (double) (worksheet.Cells[39, 10] as Range)?.Value;
 
 
             Debug.WriteLine($"Value: {cellOneValue},{cellTwoValue}");
 
             //Parse value to cell
             excelRange.Cells.set_Item(5, 3, "X65P-X85P-XKP");
-            excelRange.Cells.set_Item(6, 3,300);
+            excelRange.Cells.set_Item(6, 3, 300);
 
 
             //cellOneValue = excelRange.Item[1, 1].ToString();
             //cellTwoValue = excelRange.Item[1, 2].ToString();
-            cellOneValue = (string)(worksheet.Cells[5, 3] as Range).Value;
-            cellTwoValue = (double)(worksheet.Cells[6, 3] as Range).Value;
-            cellResult = (double)(worksheet.Cells[39, 10] as Range)?.Value;
+            cellOneValue = (string) (worksheet.Cells[5, 3] as Range).Value;
+            cellTwoValue = (double) (worksheet.Cells[6, 3] as Range).Value;
+            cellResult = (double) (worksheet.Cells[39, 10] as Range)?.Value;
 
             Debug.WriteLine($"Value: {cellOneValue},{cellTwoValue}");
             Debug.WriteLine($"Result: {cellResult}");
 
             ////wb.Save();
-            if (File.Exists(@"C:\Dev\TestProject\ExcelInterop\tmpExcel2019.xlsx"))
+            if (File.Exists(@"C:\Dev\DotNetTestProject\ExcelInterop\tmpExcel2019.xlsx"))
             {
-                File.Delete(@"C:\Dev\TestProject\ExcelInterop\tmpExcel2019.xlsx");
+                File.Delete(@"C:\Dev\DotNetTestProject\ExcelInterop\tmpExcel2019.xlsx");
             }
-            wb.SaveAs(@"C:\Dev\TestProject\ExcelInterop\tmpExcel2019", XlFileFormat.xlWorkbookDefault);
+
+            wb.SaveAs(@"C:\Dev\DotNetTestProject\ExcelInterop\tmpExcel2019", XlFileFormat.xlWorkbookDefault);
 
 
+            Marshal.ReleaseComObject(sheets);
+            Marshal.ReleaseComObject(worksheet);
 
             wb.Close(0);
             xlAppObject.Quit();
-            Marshal.ReleaseComObject(worksheet);
-            Marshal.ReleaseComObject(wb);
-
-
         }
     }
 }
---
+++ ThreadTest/ThreadTest/ThreadTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Factory.StartNew(() => {
+                Thread.CurrentThread.Name = "foo";
+                Thread.Sleep(10000);   // Use Debug + Break to see it
+            });
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine("Task Complete .. !");
+            }
+
+            task.Wait();
+
+            Console.ReadLine();
+
+        }
+    }
+}
