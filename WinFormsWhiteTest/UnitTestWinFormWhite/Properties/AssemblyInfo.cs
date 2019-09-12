70434f5d9b10b7222137bc88eb7546539867cfc1
Ali Abdulhussein <ali.abdulhussein@flexlink.com>
2019-09-12 13:44:57 +0200

    Test WinForm UI with White

---
+++ WinFormsWhiteTest/UnitTestWinFormWhite/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestWinFormWhite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestWinFormWhite")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62d79978-e725-4297-bc0b-3e5107a3e9d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WinFormsWhiteTest/UnitTestWinFormWhite/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿
+using System.IO;
+using System.Windows.Automation;
+using NUnit.Framework;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using Xunit;
+using Application = TestStack.White.Application;
+
+namespace UnitTestWinFormWhite
+{
+    public class UnitTest1
+    {
+
+        private static string applicationDirectory;
+        private static string applicationPath;
+        [Fact]
+        public void TestMethod1()
+        {
+            var window = SetUpWindow();
+
+            Button btn = window.Get<Button>("button1");
+            btn.Click();
+
+
+        }
+
+        private static Window SetUpWindow()
+        {
+            applicationDirectory = TestContext.CurrentContext.TestDirectory;
+            applicationPath = Path.Combine(applicationDirectory, "WinFormApp.exe");
+            Application application = Application.Launch(applicationPath);
+            Window window = application.GetWindow("Form1", InitializeOption.NoCache);
+            return window;
+        }
+
+        [Fact]
+        public void TestMethod2()
+        {
+            var window = SetUpWindow();
+
+            SearchCriteria searchCriteria =
+                SearchCriteria.ByAutomationId("button1")
+                    .AndControlType(ControlType.Button)
+                    .AndIndex(0);
+
+            Button btn = (Button)window.Get(searchCriteria);
+            btn.Click();
+
+        }
+    }
+}
