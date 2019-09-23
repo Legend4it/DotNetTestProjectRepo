322eb61c980e714f2c8289768b8ce3cd8d862652
Ali Abdulhussein <ali.abdulhussein@flexlink.com>
2019-09-23 15:58:54 +0200

    Use Validator to validate class properties bu data annotations

---
+++ ValidatorClassTest/ValidatorClassTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objToValidate=new Customer();
+            objToValidate.FirstName = "Hello";
+
+            var objContext = new ValidationContext(objToValidate, null, null);
+            var result =new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(objToValidate, objContext, result, true);
+
+            foreach (var str in result)
+            {
+                Console.WriteLine($"ErrorMessage:{str.ErrorMessage}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
