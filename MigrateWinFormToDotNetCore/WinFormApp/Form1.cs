02d158c1571044b5a5b9430c0486df6dceb54d5f
Ali Abdulhussein <ali.abdulhussein@flexlink.com>
2019-10-10 14:19:27 +0200

    Demo Port winforms application from dotnet FrameWork To dotnet Core

---
+++ MigrateWinFormToDotNetCore/WinFormApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetText(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GetText(sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetText(sender);
+        }
+
+        private string GetText(object sender) => sender switch
+        {
+            Button button => textBox1.Text=button.Text+34343,
+            _ =>string.Empty
+        };
+    }
+}
