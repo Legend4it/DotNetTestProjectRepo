218f82ffa596ddb8aeea383532bfe7675b06a37b
Ali Abdulhussein <ali.abdulhussein@flexlink.com>
2019-10-21 10:46:00 +0200

    Add new project

---
+++ WordDocumentGenerator/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WordDocumentGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdCreateNew = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdCreateNew
+            // 
+            this.cmdCreateNew.AutoSize = true;
+            this.cmdCreateNew.Location = new System.Drawing.Point(255, 81);
+            this.cmdCreateNew.Name = "cmdCreateNew";
+            this.cmdCreateNew.Size = new System.Drawing.Size(122, 23);
+            this.cmdCreateNew.TabIndex = 0;
+            this.cmdCreateNew.Text = "Create Word Table";
+            this.cmdCreateNew.UseVisualStyleBackColor = true;
+            this.cmdCreateNew.Click += new System.EventHandler(this.cmdCreateNew_Click);
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.AutoSize = true;
+            this.cmdCancel.Location = new System.Drawing.Point(383, 81);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(122, 23);
+            this.cmdCancel.TabIndex = 1;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(104, 42);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(612, 20);
+            this.txtFile.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select DOCX file";
+            // 
+            // cmdBrowse
+            // 
+            this.cmdBrowse.AutoSize = true;
+            this.cmdBrowse.Location = new System.Drawing.Point(722, 40);
+            this.cmdBrowse.Name = "cmdBrowse";
+            this.cmdBrowse.Size = new System.Drawing.Size(77, 23);
+            this.cmdBrowse.TabIndex = 4;
+            this.cmdBrowse.Text = "Browse";
+            this.cmdBrowse.UseVisualStyleBackColor = true;
+            this.cmdBrowse.Click += new System.EventHandler(this.cmdBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(811, 132);
+            this.Controls.Add(this.cmdBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.cmdCreateNew);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cmdCreateNew;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdBrowse;
+
+    }
+}
+
