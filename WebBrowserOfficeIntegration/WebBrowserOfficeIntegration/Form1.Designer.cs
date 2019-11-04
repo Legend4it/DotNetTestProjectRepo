e3864c41623e780a2a06f5e5de5820c5e5ebb76e
Abdulhussein <SEAAN0@flexlink.org>
2019-11-04 09:17:25 +0100

    ViewWordDocumentInAxWebBroser

--- WebBrowserOfficeIntegration/WebBrowserOfficeIntegration/Form1.Designer.cs
+++ WebBrowserOfficeIntegration/WebBrowserOfficeIntegration/Form1.Designer.cs
@@ -30,8 +30,6 @@ namespace WebBrowserOfficeIntegration
         /// </summary>
         private void InitializeComponent()
         {
-
-            
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
@@ -41,9 +39,11 @@ namespace WebBrowserOfficeIntegration
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 520);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 925);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(1226, 35);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,29 +55,29 @@ namespace WebBrowserOfficeIntegration
             // 
             // axWebBrowser1
             // 
+            this.axWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.axWebBrowser1.Enabled = true;
             this.axWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.axWebBrowser1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.axWebBrowser1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWebBrowser1.OcxState")));
-            this.axWebBrowser1.Size = new System.Drawing.Size(816, 480);
+            this.axWebBrowser1.Size = new System.Drawing.Size(1226, 925);
             this.axWebBrowser1.TabIndex = 2;
             this.axWebBrowser1.NavigateComplete2 += new AxSHDocVw.DWebBrowserEvents2_NavigateComplete2EventHandler(this.axWebBrowser1_NavigateComplete2);
-           
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(817, 624);
+            this.ClientSize = new System.Drawing.Size(1226, 960);
             this.Controls.Add(this.axWebBrowser1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Closed += new System.EventHandler(this.Form1_Closed);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axWebBrowser1)).EndInit();
             this.ResumeLayout(false);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Closed += new System.EventHandler(this.Form1_Closed);
 
         }
 
