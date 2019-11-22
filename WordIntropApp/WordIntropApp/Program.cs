cfecd18662cb323095dc9571e977a1a0ee91b5fe
FLEXLINK\SEAAN0 <ali.abdulhussein@flexlink.com>
2019-11-22 08:44:21 +0100

    Get Range position from Document

--- WordIntropApp/WordIntropApp/Program.cs
+++ WordIntropApp/WordIntropApp/Program.cs
@@ -14,7 +14,8 @@ namespace WordIntropApp
 
         private static string SetWordObject()
         {
-            var sourceFile = "C:\\Dev\\Source\\DotNetTestProjectRepo\\WordIntropApp\\WordIntropApp\\Documents\\HidenText.docx";
+            var sourceFile = "C:\\Dev\\Repo\\DotNetTestProjectRepo\\WordIntropApp\\WordIntropApp\\Documents\\HidenText.docx";
+            //var sourceFile = "C:\\Dev\\Source\\DotNetTestProjectRepo\\WordIntropApp\\WordIntropApp\\Documents\\HidenText.docx";
 
             Application oWord = new Application();
             oWord.DisplayAlerts = WdAlertLevel.wdAlertsNone;
@@ -27,30 +28,44 @@ namespace WordIntropApp
             oWordDoc.Range().TextRetrievalMode.IncludeHiddenText = true;
 
 
+            //Find range by text
+            bool wasFound = false;
+            object missing = System.Type.Missing;
+            Microsoft.Office.Interop.Word.Range rngFind = oWordDoc.Content;
+            object oEnd = Microsoft.Office.Interop.Word.WdCollapseDirection.wdCollapseEnd;
+            wasFound = rngFind.Find.Execute("Just Test Text!", ref missing, ref missing,
+                       ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                       ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
 
 
             var controls = oWordDoc.Content.ContentControls;
             int nr;
             foreach (ContentControl item in controls)
             {
+                
                 //Console.WriteLine(item.PlaceholderText.Value);
                 Console.WriteLine(item.Tag);
                 if (item.Tag!=null && item.Tag.Equals("HideMe"))
                 {
+                    //Get range Start-End then insert before Or after
+                    var rng=item.Range;
                     nr = item.Range.ListFormat.ListValue;
-                    //item.Range.ListFormat.RemoveNumbers();
+                    item.Range.ListFormat.RemoveNumbers();
                     item.Range.Font.Hidden = 1;
 
-                    item.Range.Text = "Lorem Ipsum Hide Me Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
-                    item.Range.ListFormat.ApplyNumberDefault(nr);
+                    //item.Range.ListFormat.ApplyNumberDefault(nr);
 
                 }
+
+                item.Range.InsertAfter("##################### Add text to the End Of Paragraph");
 
                 if (item.Tag!=null && item.Tag.Equals("ShowMe"))
                 {
                     item.Range.Font.Hidden = 0;
                     //item.Range.ListFormat.ApplyNumberDefault();
+
                 }
+
 
                 if (item.Tag!=null && item.Tag.Equals("HiddenTag"))
                 {
@@ -63,7 +78,7 @@ namespace WordIntropApp
                 }
             }
             
-            oWordDoc.Save();
+            oWordDoc.SaveAs2("C:\\Users\\seaan0\\Desktop\\OutPut.docx");
 
             return oWordDoc.FullName;
         }
