0106af9921772ec6e4c8914cdb6615a4932f8cc4
Ali Abdulhussein <ali.abdulhussein@flexlink.com>
2020-01-17 15:57:42 +0100

    Dotnet core docker project

---
+++ consoleappdocker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			for(var i=0;i<5;i++)
+			{
+            Console.WriteLine("Hello World!");
+			}
+        }
+    }
+}
