d7077d983a9636ce90eb5d577883b35dc6466330
SENNA0 <ali.abdulhussein@flexlink.com>
2020-04-05 18:11:28 +0200

    Arbetsprov Botex

---
+++ Botek/Botek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Botek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Timer taskTimer = new Timer(HardwareHandlerStart, null, 0, 2000);
+            Console.ReadLine();
+        }
+
+        private static void HardwareHandlerStart(object sender)
+        {
+            var task = new HardwareHandlerTask();
+
+            //Console.WriteLine($"{task.TaskId} {task.Result.Name}");
+        }
+    }
+}
